db9cc3bbb26cac119b5b300fe9a75120152bff88
xym100111100 <253274870@qq.com>
2020-04-04 10:13:55 +0800

    初始化框架!!!

---
+++ src/pages/home/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        首页
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index",
+        data(){
+            return {
+
+            }
+        },
+        created(){
+
+        },
+        methods:{
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
