4a80cde05bdaf65578d75b32c8fdf3295fab14a6
xym100111100 <253274870@qq.com>
2020-04-13 20:18:34 +0800

    添加个人首页

--- src/pages/home/login/index.vue
+++ src/pages/home/login/index.vue
@@ -1,59 +1,59 @@
 <template>
-    <div>
-        用户名：<input type="text" v-model="username" placeholder="请输入用户名" /><br>
-        密码：<input type="text" placeholder="请输入密码" v-model="password" /><br>
-        <button type="button" @click="doLogin()">登录</button>
+    <div class="page">
+        <SubHeader title="会员登录"></SubHeader>
+        <div class='main login-main'>
+            <div class='code-wrap' style="margin-top:0px"><input type="text" placeholder="手机号" v-model="cellphone"/></div>
+            <div class='password-wrap'>
+                <div class='password'><input :type="isOpen?'text':'password'" placeholder="密码" v-model="password" /></div>
+                <div class='switch-wrap'>
+                    <van-switch v-model="isOpen" active-color="#EB1625" />
+                </div>
+            </div>
+            <div class='sure-btn' >登录</div>
+            <div class="fastreg-wrap">
+                <div><img src="../../../assets/images/home/index/forget.png" alt="忘记密码"/> 忘记密码</div>
+                <div @click="$router.push('/reg')" ><img src="../../../assets/images/home/index/reg.png" alt="忘记密码" /> 快速注册</div>
+        </div>
+        </div>
     </div>
 </template>
 
 <script>
-    // import {request} from '../../assets/js/utils/request';
-    import {mapActions} from "vuex";
+    import Vue from 'vue';
+    import {mapActions} from 'vuex';
+    import { Switch,Toast } from 'vant';
+    import SubHeader from "../../../components/sub_header";
+    Vue.use(Switch);
+    Vue.use(Toast);
     export default {
-        name: "login",
         data(){
             return {
-                username:"",
+                isOpen:false,
+                cellphone:"",
                 password:""
             }
         },
+        components:{
+            SubHeader
+        },
         methods:{
-            ...mapActions({
-                login:"user/login"
-            }),
-            doLogin(){
-                if (this.username.match(/^\s*$/)){
-                    alert("请输入用户名");
-                    return;
-                }
-                if (this.password.match(/^\s*$/)){
-                    alert("请输入密码");
-                    return;
-                }
-                this.login({cellphone:this.username,password:this.password,success:(res)=>{
-                        // console.log(res);
-                        if (res.code===200){
-                            this.$router.go(-1)
-                        } else{
-                            alert(res.data);
-                        }
-                    }});
-                // request(process.env.VUE_APP_API+"/home/user/pwdlogin?token=1ec949a15fb709370f","post",{cellphone:this.username,password:this.password}).then(res=>{
-                //     // console.log(res);
-                //     if (res.code===200){
-                //         localStorage['uid']=res.data.uid;
-                //         localStorage['nickname']=res.data.nickname;
-                //         localStorage['isLogin']=true;
-                //         this.$router.go(-1);
-                //     } else{
-                //         alert(res.data);
-                //     }
-                // })
-            }
         }
     }
 </script>
 
 <style scoped>
-
+    .page{width:100%;height:100vh;background-color: #ffffff;}
+    .main{width:100%;padding-top:1.4rem;}
+    .cellphone-wrap{width:90%;height:0.84rem;margin:0 auto;display: flex;display: -webkit-flex;justify-content: space-between;-webkit-justify-content: space-between;}
+    .cellphone-wrap .cellphone{width:62%;height:100%;border:#EFEFEF solid 1px;border-radius: 2px;}
+    .cellphone-wrap .cellphone input,.code-wrap input,.password-wrap .password input{width:92%;height:95%;font-size:0.28rem;padding-left:0.2rem;}
+    .cellphone-wrap .code-btn{width:35%;height:0.82rem;background:#EAEAEA;color:#717376;border:#EAEAEA solid 1px;border-radius: 2px;font-size:0.28rem;text-align:center;line-height:0.82rem;}
+    .cellphone-wrap .code-btn.success{background:#FFFFFF;border:1px solid #EB1625;color:#EB1625}
+    .code-wrap{width:90%;height:0.84rem;margin:0 auto;border-radius: 2px;border:#EFEFEF solid 1px;margin-top:0.4rem;}
+    .password-wrap{width:90%;height:0.84rem;margin:0 auto;border-radius: 2px;border:#EFEFEF solid 1px;margin-top:0.4rem;display:flex;display: -webkit-flex;}
+    .password-wrap .password{width:80%;height:100%;}
+    .password-wrap .switch-wrap{width:20%;margin-top:0.1rem;}
+    .sure-btn{width:85%;height:0.8rem;margin:0 auto;background:#EB1625;font-size:0.36rem;color:#FFFFFF;line-height:0.8rem;text-align: center;margin-top:0.4rem;border-radius: 4px;}
+    .fastreg-wrap{width:85%;display:flex;display:-webkit-flex;justify-content: space-between;-webkit-justify-content: space-between;font-size:0.24rem;margin:0 auto;margin-top:0.3rem;}
+    .fastreg-wrap img{width:0.32rem;height:0.32rem;vertical-align: middle;}
 </style>
--- src/pages/user/ucenter/index.vue
+++ src/pages/user/ucenter/index.vue
@@ -1,23 +1,100 @@
 <template>
     <div>
-        我的
+        <div class='sub-header'>
+            <div class='back'></div>
+            <div class='title'>个人中心</div>
+            <div class="right-btn hide">保存</div>
+        </div>
+        <div class='user-info-wrap'>
+            <div class='head'>
+                <img src="../../../assets/images/user/my/default-head.png" alt="" />
+            </div>
+            <div class='nickname'>昵称</div>
+            <div class='points'>我的积分：10</div>
+        </div>
+        <div class='order-name-wrap'>
+            <div class='order-name'>全部订单</div>
+            <div class='show-order' >查看全部订单 &gt;</div>
+        </div>
+        <div class='order-status-wrap'>
+            <div class='item'>
+                <div class='icon wait'></div>
+                <div class='text'>待支付</div>
+            </div>
+            <div class='item'>
+                <div class='icon take'></div>
+                <div class='text'>待收货</div>
+            </div>
+            <div class='item'>
+                <div class='icon comment'></div>
+                <div class='text'>待评价</div>
+            </div>
+        </div>
+        <div class="menu-list-wrap">
+            <ul >
+                <li>个人资料</li>
+                <li></li>
+            </ul>
+            <ul >
+                <li>收货地址</li>
+                <li></li>
+            </ul>
+            <ul>
+                <li>绑定手机</li>
+                <li></li>
+            </ul>
+            <ul>
+                <li>修改密码</li>
+                <li></li>
+            </ul>
+            <ul>
+                <li>我的收藏</li>
+                <li></li>
+            </ul>
+            <div class='btn'>登录/注册</div>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "ucenter",
-        data(){
-            return {
-
-            }
+        mounted(){
+            document.title=this.$route.meta.title
         },
-        created(){
-
+        methods:{
+            
         }
     }
 </script>
 
 <style scoped>
+    .sub-header{width:100%;height:1rem;background-color:#FFFFFF;display: flex;display:-webkit-flex;align-items: center;-webkit-align-items: center;border-bottom: 1px solid #EFEFEF;position: fixed;z-index: 10;left:0;top:0;}
+    .sub-header .back{width:0.8rem;height:0.8rem;background-image:url("../../../assets/images/home/goods/back.png");background-size:100%;background-repeat: no-repeat;background-position: center;}
+    .sub-header .title{width:79%;height:auto;font-size:0.32rem;text-align: center;}
+    .sub-header .right-btn{width:auto;height:auto;font-size:0.32rem;}
+
+    .user-info-wrap{width:100%;height:2.8rem;background-image:url("../../../assets/images/user/my/mybg.png");background-size:100%;background-repeat:no-repeat;background-position: top;margin-top:1.02rem;position: relative;z-index:1;}
+    .user-info-wrap .head{width:1.2rem;height:1.2rem;position: absolute;z-index:1;left:5%;bottom:15%;}
+    .user-info-wrap .head img{width:100%;height:100%;border-radius: 100%;}
+    .user-info-wrap .nickname{width:auto;height:auto;white-space: nowrap;position:absolute;z-index:1;left:27%;bottom:35%;font-size:0.24rem;color:#FFFFFF;}
+    .user-info-wrap .points{width:auto;height:auto;white-space: nowrap;position:absolute;z-index:1;left:27%;bottom:20%;font-size:0.24rem;color:#FFFFFF;}
+
+    .order-name-wrap{width:100%;height:0.8rem;background-color:#FFFFFF;display:flex;display:-webkit-flex;justify-content: space-between;-webkit-justify-content: space-between;align-items: center;-webkit-align-items: center;overflow:hidden;margin-bottom:1px;}
+    .order-name-wrap .order-name{font-size:0.28rem;margin-left:3%;}
+    .order-name-wrap .show-order{font-size:0.24rem;margin-right:3%;}
+
+    .order-status-wrap{width:80%;height:1.4rem;background-color:#FFFFFF;display:flex;display:-webkit-flex;justify-content: space-between;-webkit-justify-content: space-between;align-items: center;-webkit-align-items:center;padding-left:10%;padding-right:10%;}
+    .order-status-wrap .item{width:1rem;height:auto;}
+    .order-status-wrap .item .icon{width:0.5rem;height:0.5rem;margin:0 auto;}
+    .order-status-wrap .item .icon.wait{background-image:url("../../../assets/images/user/my/pay.png");background-size:100%;background-repeat:no-repeat;background-position: center;}
+    .order-status-wrap .item .icon.take{background-image:url("../../../assets/images/user/my/shouhuo.png");background-size:100%;background-repeat:no-repeat;background-position: center;}
+    .order-status-wrap .item .icon.comment{background-image:url("../../../assets/images/user/my/comment.png");background-size:100%;background-repeat:no-repeat;background-position: center;}
+    .order-status-wrap .item .text{width:100%;font-size:0.24rem;text-align:center;}
+
+    .menu-list-wrap{width:100%;height:7rem;background-color:#FFFFFF;margin-top:0.3rem;}
+    .menu-list-wrap ul{width:87%;height:0.8rem;border-bottom:1px solid #EFEFEF;display:flex;display:-webkit-flex;justify-content: space-between;-webkit-justify-content: space-between;align-items: center;-webkit-align-items: center;padding-left:10%;padding-right:3%;}
+    .menu-list-wrap ul li:nth-child(1){font-size:0.32rem;}
+    .menu-list-wrap ul li:nth-child(2){width:0.4rem;height:0.4rem;background-image: url("../../../assets/images/common/right_arrow.png");background-size:100%;background-repeat: no-repeat;background-position: center;}
+    .menu-list-wrap .btn{width:70%;height:0.8rem;margin:0 auto;background-color:#E51B19;border-radius: 5px;font-size:0.32rem;color:#FFFFFF;text-align:center;line-height:0.8rem;margin-top:0.4rem;}
 
 </style>
